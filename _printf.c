fb621cef422c3402385e923ab346231a7c66eda1
Elizabeth Salako <waleade602@gmail.com>
2022-04-17 02:36:18 +0000

    fixbetty for task 0

--- _printf.c
+++ _printf.c
@@ -1,8 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -16,29 +14,21 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_string - prints an array of string characters
+ * @str: the variable to print
+ *
+ */
+
 void print_string(char *str)
 {
 	int i = 0;
+
 	do {
-		_putchar( str[i]);
+		_putchar(str[i]);
 		i++;
 	} while (str[i] != '\0');
 }
-
-/*void write_int(int n)
-{
-	int tmp = n, i = 0, j = 0; 
-	while (n % 10 > 9)
-	{
-		i++;
-		tmp = tmp % 10;
-	}
-	
-	for (j = tmp; j > 0; j--)
-		_putchar(48 + (n % (int)pow(10, tmp)));
-	_putchar(48 + tmp);
-
-}*/
 
 /**
  * _printf - Produces output according to a format
@@ -46,16 +36,10 @@ void print_string(char *str)
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
-{	
+{
 	va_list ap;
-	unsigned int j = 0, n = 0; /*i = 0;*/
-	
-	/*while (format[i] != '\0')
-	{	
-		if (format[i] == '%')
-			n++;
-		i++;
-	}*/
+	unsigned int j = 0, n = 0;
+
 	va_start(ap, format);
 	while (format[j] != '\0')
 	{
@@ -74,15 +58,11 @@ int _printf(const char *format, ...)
 					break;
 			}
 
-			/*if (format[j + 1] == 'c')
-				_putchar(va_arg(ap, int));
-			else if (format[j + 1] == 's')
-				print_string(va_arg(ap, char *));*/
 			j++;
 		}
-		else 
+		else
 		{
-			_putchar(format[j]); 
+			_putchar(format[j]);
 		}
 		j++;
 	}
